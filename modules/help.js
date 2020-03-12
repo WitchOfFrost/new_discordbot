ae36c795dcd21e81c5087a7c5400041f64362a1a
Christen Tautz <unlimited.gamer@gmx.de>
2020-03-12 23:39:24 +0100

    Added help switch for shibe

--- modules/help.js
+++ modules/help.js
@@ -3,19 +3,18 @@ var strings = new json_reload("./strings.json");
 const config = require('./../config.json');
 const module_index = require("./../module_index.js")
 
-module.exports.help = async function (obj_sub) {
+module.exports.help = async function(obj_sub) {
 
     switch (obj_sub.dc_args[1]) {
-        default:
-            embed_data_help = {
-                embed: {
-                    title: strings.help_strings.help_title,
-                    description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.help_desc)
-                }
+        default: embed_data_help = {
+            embed: {
+                title: strings.help_strings.help_title,
+                description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.help_desc)
             }
-            break;
+        }
+        break;
         case "ping":
-            embed_data_help = {
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.ping_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.ping_desc)
@@ -23,7 +22,7 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case "color":
-            embed_data_help = {
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.color_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.color_desc)
@@ -31,9 +30,10 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case ("cat"):
-        case ("dog"):
-        case ("fox"):
-            embed_data_help = {
+                case ("dog"):
+                case ("fox"):
+                case ("shibe"):
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.animal_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.animal_desc)
@@ -41,17 +41,17 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case (`neko`):
-        case (`hug`):
-        case (`fluff`):
-        case (`wag`):
-        case (`kiss`):
-        case (`pat`):
-        case (`cuddle`):
-        case (`poke`):
-        case (`wallpaper`):
-        case (`gecg`):
-        case (`8ball`):
-            embed_data_help = {
+                case (`hug`):
+                case (`fluff`):
+                case (`wag`):
+                case (`kiss`):
+                case (`pat`):
+                case (`cuddle`):
+                case (`poke`):
+                case (`wallpaper`):
+                case (`gecg`):
+                case (`8ball`):
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.neko_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.neko_desc)
@@ -59,7 +59,7 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case (`suck`):
-            embed_data_help = {
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.lewdneko_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.lewdneko_desc)
@@ -67,8 +67,8 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case (`agdq`):
-        case (`sgdq`):
-            embed_data_help = {
+                case (`sgdq`):
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.agdq_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.agdq_desc),
@@ -83,7 +83,7 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case (`urban`):
-            embed_data_help = {
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.urban_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.urban_desc),
@@ -92,8 +92,8 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case (`spotify`):
-        case (`s`):
-            embed_data_help = {
+                case (`s`):
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.spotify_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.spotify_desc),
@@ -102,8 +102,8 @@ module.exports.help = async function (obj_sub) {
             }
             break;
         case (`twitter`):
-        case (`t`):
-            embed_data_help = {
+                case (`t`):
+                embed_data_help = {
                 embed: {
                     title: strings.help_strings.twitter_title,
                     description: module_index.globalfunctions_loader.str_replace("%prefix%", config.bot_config.prefix, strings.help_strings.twitter_desc),
@@ -115,27 +115,27 @@ module.exports.help = async function (obj_sub) {
     obj_sub.dc_msg.channel.send(embed_data_help);
 };
 
-module.exports.commands = async function (obj_sub) {
+module.exports.commands = async function(obj_sub) {
     obj_sub.dc_msg.channel.send({
         embed: {
             title: strings.command_strings.command_title,
             description: strings.command_strings.command_desc,
             fields: [{
-                name: strings.command_strings.utility_title,
-                value: strings.command_strings.utility_desc
-            },
-            {
-                name: strings.command_strings.image_title,
-                value: strings.command_strings.image_desc
-            },
-            {
-                name: strings.command_strings.nsfw_title,
-                value: strings.command_strings.nsfw_desc
-            },
-            {
-                name: strings.command_strings.misc_title,
-                value: strings.command_strings.misc_desc
-            }
+                    name: strings.command_strings.utility_title,
+                    value: strings.command_strings.utility_desc
+                },
+                {
+                    name: strings.command_strings.image_title,
+                    value: strings.command_strings.image_desc
+                },
+                {
+                    name: strings.command_strings.nsfw_title,
+                    value: strings.command_strings.nsfw_desc
+                },
+                {
+                    name: strings.command_strings.misc_title,
+                    value: strings.command_strings.misc_desc
+                }
             ]
         }
     });
